e197f5d027ad00fd3cad2ad32f51fe0fd54fe1d9
expert975 <rodrigocomputadores125@hotmail.com>
2017-08-29 08:46:29 -0300

    commit

---
+++ dist.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	double x0, y0, x1, y1, dist = 0;
+
+	printf("Enter x0: ");
+	scanf("%lf", &x0);
+
+	printf("Enter y0: ");
+	scanf("%lf", &y0);
+
+	printf("Enter x1: ");
+	scanf("%lf", &x1);
+
+	printf("Enter y1: ");
+	scanf("%lf", &y1);
+
+	dist = sqrt(pow((x0 - y0), 2) + pow((x1 - y1), 2));
+
+	printf("Euclidean distance: %f", dist);
+
+	return 0;
+}
