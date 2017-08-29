fdff2cccfc593e0081b3248e863ca8a4f08fa060
expert975 <rodrigocomputadores125@hotmail.com>
2017-08-29 09:16:03 -0300

    commit

---
+++ 3digits.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int number, hundreds, tens, ones = 0;
+
+	printf("Enter a number: ");
+	scanf("%d", &number);
+
+	hundreds = number/100;
+	tens = (number - hundreds*100)/10;
+	ones = number - hundreds*100 - tens*10;
+
+	printf("Hundreds: %d, tens: %d, ones: %d.\n", hundreds, tens, ones);
+	printf("Number backwards: %d%d%d\n", ones, tens, hundreds);
+
+	return 0;
+}
