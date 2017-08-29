40ff45e3682101efb3d29672f1c6bd76ca3e4544
expert975 <rodrigocomputadores125@hotmail.com>
2017-08-29 08:58:59 -0300

    commit

--- dist.c
+++ dist.c
@@ -8,18 +8,18 @@ int main()
 	printf("Enter x0: ");
 	scanf("%lf", &x0);
 
-	printf("Enter y0: ");
-	scanf("%lf", &y0);
-
 	printf("Enter x1: ");
 	scanf("%lf", &x1);
+
+	printf("Enter y0: ");
+	scanf("%lf", &y0);
 
 	printf("Enter y1: ");
 	scanf("%lf", &y1);
 
 	dist = sqrt(pow((x0 - y0), 2) + pow((x1 - y1), 2));
 
-	printf("Euclidean distance: %f", dist);
+	printf("Euclidean distance: %f\n", dist);
 
 	return 0;
 }
