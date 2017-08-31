a5db505e0c7fee53f0def901b425e6ca1d8dc79b
Convidado <rodrigocomputadores125@hotmail.com>
2017-08-31 10:29:21 -0300

    commit

---
+++ age.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+	int birthYear = 0;
+	int age = 0;
+	bool error = true;
+
+	while(error)
+	{
+		printf("Enter your birthyear: ");
+		scanf("%d", &birthYear);
+
+		if (birthYear <= 2017 && birthYear > 0)
+			error = false;
+	}
+
+	age = 2007 - birthYear;
+
+	if (age >= 0)
+		printf("Your age at 2007 was: %d\n", age);
+	else
+		printf("You were not born at 2007\n");
+
+	return 0;
+}
